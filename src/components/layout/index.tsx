1c1c2b4fb039d3b0dce975132aff3cbf3fb5b49c
Juan Guilherme <asjuanguilherme@gmail.com>
2022-11-05 23:16:16 -0300

    feat: :construction: wip - menu mobile

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,12 @@
 import * as S from './styles'
 
 // Types
-import { ReactNode, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 // Components
 import Footer from './Footer'
 import Header from './Header'
+import useScreenDimensions from 'hooks/useScreenDimensions'
 
 export type LayoutProps = {
   children: ReactNode
@@ -13,16 +14,21 @@ export type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   const [menuOpened, setMenuOpened] = useState(false)
+  const { screen, breakpoints } = useScreenDimensions()
+  const isMobile = screen.width < breakpoints.tablet
 
   const menuToggle = () => {
     setMenuOpened(state => !state)
   }
 
   return (
-    <S.Wrapper>
-      <Header menuOpened={menuOpened} menuToggle={menuToggle} />
-      <S.Main>{children}</S.Main>
-      <Footer />
+    <S.Wrapper menuOpened={menuOpened} isMobile={isMobile}>
+      <S.PageWrapper onClick={() => menuOpened && setMenuOpened(false)}>
+        <Header menuOpened={menuOpened} menuToggle={menuToggle} />
+        <S.Main>{children}</S.Main>
+        <Footer />
+      </S.PageWrapper>
+      {isMobile && menuOpened && <S.MenuMobileWrapper>red</S.MenuMobileWrapper>}
     </S.Wrapper>
   )
 }
--- src/components/layout/styles.ts
+++ src/components/layout/styles.ts
@@ -1,12 +1,59 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import designSystemOptions from 'styles/designSystemOptions'
+
+const { borderRadius, spacing } = designSystemOptions
 
 export const Main = styled.main`
   flex: 1;
 `
 
-export const Wrapper = styled.div`
+export const MenuMobileWrapper = styled.div`
+  width: 65%;
+  position: absolute;
+  right: ${spacing.components.small};
+  top: 50%;
+  transform: translateY(-50%);
+  background: red;
+  padding: ${spacing.components.small};
+  z-index: 1;
+`
+
+export const PageWrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  left: 0;
+  top: 0;
+  overflow: auto;
+  overflow-x: hidden;
+  background: ${props => props.theme.colors.layers[0].background};
+  transition: 0.3s ease-out;
+  transition-property: transform, left;
+
   display: flex;
   flex-direction: column;
   height: 100%;
-  background: ${props => props.theme.colors.layers[0].background};
+  z-index: 2;
+`
+
+const menuOpenedStyle = css`
+  ${PageWrapper} {
+    border-radius: ${borderRadius.medium};
+    transform: scale(0.8);
+    left: -60%;
+  }
+`
+
+export const Wrapper = styled.div<{
+  menuOpened: boolean
+  isMobile: boolean
+}>`
+  height: 100%;
+  background: linear-gradient(
+    -90deg,
+    ${props => props.theme.colors.main.themeGradient.normal} 50%,
+    ${props => props.theme.colors.main.themeGradient.dark}
+  );
+
+  ${props => props.menuOpened && props.isMobile && menuOpenedStyle}
 `
