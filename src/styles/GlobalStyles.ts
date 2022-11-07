7cbbe74e0b3c47ad158550c0bb3985506d84b7cd
Juan Guilherme <asjuanguilherme@gmail.com>
2022-11-07 10:35:56 -0300

    feat: :sparkles: section components + fix global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,4 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
+import { screens } from './screens'
+import designSystemOptions from 'styles/designSystemOptions'
+
+const { spacing, font } = designSystemOptions
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -13,13 +17,29 @@ const GlobalStyles = createGlobalStyle`
 
   html {
     color: ${props => props.theme.colors.text};
-    font-weight: 500;
+    font-weight: ${font.weight.normal};
   }
   
   html, body, #__next {
     height: 100%;
   }
   
+  h1,h2,h3,h4,h5,h6 {
+    margin-bottom: ${spacing.components.medium};
+
+    ${screens.tablet} {
+      margin-bottom: ${spacing.components.large};
+    }
+  }
+
+  p {
+    margin-bottom: ${spacing.components.medium};
+    line-height: 1.5em;
+  } 
+
+  strong {
+    font-weight: ${font.weight.semibold};
+  }
 `
 
 export default GlobalStyles
