5e362f9b43bc6571a329e54644d2d7225d2dca50
Juan Guilherme <asjuanguilherme@gmail.com>
2022-11-07 20:40:45 -0300

    feat: :construction: wip - project cards carousel

--- src/components/section/HomeProjectsSection/index.tsx
+++ src/components/section/HomeProjectsSection/index.tsx
@@ -1,8 +1,14 @@
 import * as S from './styles'
+import { breakpoints } from 'styles/screens'
 import { SectionHeading, SectionWrapper } from 'components/shared/Section'
 import Container from 'components/shared/Container'
 import projectsListMockup from 'mockups/projects'
 import ProjectCard from 'components/shared/ProjectCard'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
+import designSystemOptions from 'styles/designSystemOptions'
 
 export type HomeProjectsSectionProps = {}
 
@@ -11,14 +17,35 @@ const HomeProjectsSection = ({}: HomeProjectsSectionProps) => {
     <SectionWrapper id="#projects">
       <Container>
         <SectionHeading>Ultimos Projetos</SectionHeading>
-        <S.ProjectsGrid>
-          {projectsListMockup.map(project => (
-            <li key={project.id}>
-              <ProjectCard {...project} layer={1} />
-            </li>
-          ))}
-        </S.ProjectsGrid>
       </Container>
+      <S.ProjectsCarouselContainer>
+        <Swiper
+          slidesOffsetAfter={24}
+          slidesOffsetBefore={24}
+          spaceBetween={24}
+          breakpoints={{
+            [breakpoints.mobileS]: {
+              slidesPerView: 1.3
+            },
+            [breakpoints.tabletS]: {
+              slidesPerView: 2.3,
+              slidesPerGroup: 2
+            },
+            [breakpoints.laptop]: {
+              slidesPerView: 3,
+              slidesPerGroup: 3,
+              slidesOffsetAfter: 0,
+              slidesOffsetBefore: 0
+            }
+          }}
+        >
+          {projectsListMockup.map(project => (
+            <SwiperSlide key={project.id}>
+              <ProjectCard {...project} layer={1} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </S.ProjectsCarouselContainer>
     </SectionWrapper>
   )
 }
--- src/components/section/HomeProjectsSection/styles.ts
+++ src/components/section/HomeProjectsSection/styles.ts
@@ -1,19 +1,9 @@
 import styled from 'styled-components'
-import designSystemOptions from 'styles/designSystemOptions'
-import { screens } from 'styles/screens'
+import Container from 'components/shared/Container'
+import { breakpoints } from 'styles/screens'
 
-const { spacing } = designSystemOptions
-
-export const ProjectsGrid = styled.ul`
-  display: grid;
-  gap: ${spacing.components.medium};
-  list-style: none;
-
-  ${screens.tabletS} {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  ${screens.tablet} {
-    grid-template-columns: repeat(3, 1fr);
+export const ProjectsCarouselContainer = styled(Container)`
+  @media screen and (max-width: ${breakpoints.laptop + 'px'}) {
+    padding: 0;
   }
 `
