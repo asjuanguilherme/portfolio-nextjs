218c863495a3cdb887704573321927a007bd96b5
Juan Guilherme <asjuanguilherme@gmail.com>
2022-12-01 20:22:44 -0300

    fix: :wrench: adjust get cms media url fn

--- src/services/cms/utils.ts
+++ src/services/cms/utils.ts
@@ -3,5 +3,8 @@ import { CMS_URL } from './config'
 import ImageDto from './types/ImageDto'
 
 export const getCmsMediaUrl = (media: StrapiData<ImageDto>) => {
-  return `${CMS_URL}${media.attributes.url}`
+  if (process.env.NEXT_PUBLIC_IMAGES_DOMAIN === 'localhost')
+    return `${CMS_URL}${media.attributes.url}`
+
+  return media.attributes.url
 }
