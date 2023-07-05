7b6ce162fb5595e153b2e9d02a298c3c063ea8bc
Juan Guilherme <asjuanguilherme@gmail.com>
2023-07-05 14:56:26 -0300

    :poop: adjust https agent for axios requests

--- src/services/cms/config.ts
+++ src/services/cms/config.ts
@@ -1,7 +1,15 @@
 import axios from 'axios'
+import https from 'https'
 
 export const CMS_URL = process.env.NEXT_PUBLIC_CMS_URL
 
 if (!CMS_URL) throw new Error('You must insert the CMS_URL env value')
 
-export const cms = axios.create({ baseURL: `${CMS_URL}/api` })
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false
+})
+
+export const cms = axios.create({
+  baseURL: `${CMS_URL}/api`,
+  httpsAgent
+})
